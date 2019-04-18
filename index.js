d7ffec1994488c2f942d2165ea4b32118d8c048e
geoffrey woodcock <theatomproject@gmail.com>
2019-04-18 20:28:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+const driver = {
+};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  debugger;
+  // const newDriver = {...driver};
+  // newDriver[key] = value;
+  // return newDriver;
+}
+
+
+// function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+//   driver[key] = value;
+//   return driver;
+// }
+
